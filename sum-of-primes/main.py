bf8e1b9fec12fe04de479e36d93928be2ff32ebf
Tim Foster <timsprimary@gmail.com>
2017-08-25 14:10:04 +0200

    Initial commit

---
+++ sum-of-primes/main.py
@@ -0,0 +1,35 @@
+# This program will find the sum of all the prime numbers up to a given number
+
+import math
+
+def main():
+    num = int(input("Please enter a number: "))
+    while (num < 0):
+        num = int(input("Please enter a positive integer: "))
+
+    total = 0
+    for i in range(0, num):
+        if (isPrime(i)):
+            total += i
+
+    print("The sum of primes up to " + str(num) + " is : " + str(total))
+
+def isPrime(num):
+    if (num <= 1):
+        return False
+    elif (num <= 3):
+        return True
+    elif (num % 3 == 0 or num % 2 == 0):
+        return False
+
+    sqrt = math.ceil(math.sqrt(num))
+    i = 5
+
+    while (i <= sqrt):
+        if (num % i == 0):
+            return False
+        i += 1
+
+    return True
+
+main()
